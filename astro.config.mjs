98696a942ae367f9bd0dc6f71300d00b609afc9c
Han <han109k@gmail.com>
2023-07-16 15:55:40 +0300

    astro config and tsconfig changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  base: '/',
+  integrations: [preact()],
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
